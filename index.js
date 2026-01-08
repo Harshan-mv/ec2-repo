8736e5e443f87fd42336cbc0b29e9720b2509216
HaruDEMO <hv754310@gmail.com>
2026-01-08 14:47:15 +0530

    Add Node app, CI/CD workflow, and gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.send("Hello from AWS EC2 🚀 Node.js is running on https!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
